820be3a7619aa482847867c54af668bc91cac804
Annie Ton-Nu <annieqton@gmail.com>
2016-12-01 14:25:14 -0800

    add function for 7 questions

--- app.js
+++ app.js
@@ -9,85 +9,105 @@ console.log('User\'s name is: ' + userName);
 
 if (document.getElementById('aboutme')) {
 
-//   var response1 = prompt('Can you guess what my favorite movies are? (Yes/No)').toLowerCase();
-//   console.log('Can the user guess my favorite movies: ' + response1);
+//Question 1
+function questionOne(){
+  var response1 = prompt('Can you guess what my favorite movies are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite movies: ' + response1);
   var correctAnswers = 0;
-//
-//   if (response1 === 'yes' || response1 === 'y') {
-//     alert('Yes, I love Fight Club, Trainspotting, Inglorious Basterds, and more!');
-//     correctAnswers += 1;
-//   } else if (response1 === 'no' || response1 === 'n'){
-//     alert('That\'s ok. You can learn what they are in a bit.');
-//   } else {
-//     alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
-//   }
-//
-//   var response2 = prompt('Can you guess what my favorite TV Shows are? (Yes/No)').toLowerCase();
-//   console.log('Can the user guess my favorite tv shows: ' + response2);
-//
-//   if (response2 === 'yes' || response2 === 'y') {
-//     alert('Yes, I love It\'s Always Sunny in Philadelphia, Archer, Rick and Morty, and more!');
-//     correctAnswers += 1;
-//   } else if (response2 === 'no' || response2 === 'n'){
-//     alert('That\'s ok. You can learn what they are in a bit.');
-//   } else {
-//     alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
-//   }
-//
-//   var response3 = prompt('Can you guess what my favorite Books are? (Yes/No)').toLowerCase();
-//   console.log('Can the user guess my favorite books: ' + response3);
-//
-//   if (response3 === 'yes' || response3 === 'y') {
-//     alert('Yes, I love The Lord of the Rings Trilogy, The Hobbit, and The Silmarillion, The Harry Potter Series, Ender\'s Game and Series, and more!');
-//     correctAnswers += 1;
-//   } else if (response3 === 'no' || response3 === 'n'){
-//     alert('That\'s ok. You can learn what they are in a bit.');
-//   } else {
-//     alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
-//   }
-//
-//   var response4 = prompt('Can you guess what my favorite random stuff/media are? (Yes/No)').toLowerCase();
-//   console.log('Can the user guess my favorite random stuff: ' + response4);
-//
-//   if (response4 === 'yes' || response4 === 'y') {
-//     alert('Yes, I love Dungeons and Dragons (D&D) related things (i.e. books, blurbs, etc.), Critical Role -online D&D adventure (Twitch Show), Random/Useless information acquired by following "internet rabbit-holes", and more!');
-//     correctAnswers += 1;
-//   } else if (response4 === 'no' || response4 === 'n'){
-//     alert('That\'s ok. You can learn what they are in a bit.');
-//   } else {
-//     alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
-//   }
-//
-//   var response5 = prompt('Can you tell me what your favorite things are?').toLowerCase();
-//   console.log('Some of the user\'s favotire things: ' + response5);
-//
-//   if (isNaN(response5)) {
-//     document.write(response5 + ' <--- Wow! These seem pretty cool!');
-//   } else {
-//     document.write('That\'s ok, you don\'t wanna share, I understand.');
-//   }
-// //Question 6
-//   var response6 = 0;
+
+  if (response1 === 'yes' || response1 === 'y') {
+    alert('Yes, I love Fight Club, Trainspotting, Inglorious Basterds, and more!');
+    correctAnswers += 1;
+  } else if (response1 === 'no' || response1 === 'n'){
+    alert('That\'s ok. You can learn what they are in a bit.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
+  }
+}
+
+//Question 2
+function questionTwo(){
+  var response2 = prompt('Can you guess what my favorite TV Shows are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite tv shows: ' + response2);
+
+  if (response2 === 'yes' || response2 === 'y') {
+    alert('Yes, I love It\'s Always Sunny in Philadelphia, Archer, Rick and Morty, and more!');
+    correctAnswers += 1;
+  } else if (response2 === 'no' || response2 === 'n'){
+    alert('That\'s ok. You can learn what they are in a bit.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
+  }
+}
+
+// Question 3
+function questionThree(){
+  var response3 = prompt('Can you guess what my favorite Books are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite books: ' + response3);
+
+  if (response3 === 'yes' || response3 === 'y') {
+    alert('Yes, I love The Lord of the Rings Trilogy, The Hobbit, and The Silmarillion, The Harry Potter Series, Ender\'s Game and Series, and more!');
+    correctAnswers += 1;
+  } else if (response3 === 'no' || response3 === 'n'){
+    alert('That\'s ok. You can learn what they are in a bit.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
+  }
+}
+
+// Question 4
+function questionFour() {
+  var response4 = prompt('Can you guess what my favorite random stuff/media are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite random stuff: ' + response4);
+
+  if (response4 === 'yes' || response4 === 'y') {
+    alert('Yes, I love Dungeons and Dragons (D&D) related things (i.e. books, blurbs, etc.), Critical Role -online D&D adventure (Twitch Show), Random/Useless information acquired by following "internet rabbit-holes", and more!');
+    correctAnswers += 1;
+  } else if (response4 === 'no' || response4 === 'n'){
+    alert('That\'s ok. You can learn what they are in a bit.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
+  }
+}
+
+//Question 5
+function questionFive () {
+  var response5 = prompt('Can you tell me what your favorite things are?').toLowerCase();
+  console.log('Some of the user\'s favotire things: ' + response5);
+
+  if (isNaN(response5)) {
+    document.write(response5 + ' <--- Wow! These seem pretty cool!');
+  } else {
+    document.write('That\'s ok, you don\'t wanna share, I understand.');
+  }
+}
+
+//Question 6
+function questionSix() {
+  var response6 = 0;
   var counter = 0;
-//   while (response6 !== 26 && counter < 4){
-//     response6 = parseInt(prompt('Q: In how many hours does a chicken lay one egg? \n Hint: It\'s less than 30!\nYou have 4 tries!'));
-//     console.log('userName thinks the answer is: ' + response6);
-//     counter += 1;
-//     if (response6 > 26){
-//       alert('Your number it greater than the answer!');
-//     } else if (response6 === 26) {
-//       alert('You\'re right, chickens take ' + response6 + ' hours to lay one egg!');
-//     correctAnswers += 1;
-//     } else if (isNaN(response6)) {
-//       alert('Hey! Don\'t be a smartass!');
-//     } else {
-//       alert('Your number is lesser than the answer!');
-//     }
-//   }
-//   if (response6 !== 26){
-//     alert('BTW chickens take 26 hours to lay one egg!');
-//   }
+  while (response6 !== 26 && counter < 4){
+    response6 = parseInt(prompt('Q: In how many hours does a chicken lay one egg? \n Hint: It\'s less than 30!\nYou have 4 tries!'));
+    console.log('userName thinks the answer is: ' + response6);
+    counter += 1;
+    if (response6 > 26){
+      alert('Your number it greater than the answer!');
+    } else if (response6 === 26) {
+      alert('You\'re right, chickens take ' + response6 + ' hours to lay one egg!');
+    correctAnswers += 1;
+    } else if (isNaN(response6)) {
+      alert('Hey! Don\'t be a smartass!');
+    } else {
+      alert('Your number is lesser than the answer!');
+    }
+  }
+  if (response6 !== 26){
+    alert('BTW chickens take 26 hours to lay one egg!');
+  }
+}
+
   //Question 7
+  function questionSeven() {
   var countries = ['US', 'ENGLAND', 'FRANCE', 'BELGIUM', 'NETHERLAND', 'GERMANY','ITALY', 'HUNGARY', 'CZECH REPUBLIC', 'ISRAEL', 'CHINA', 'THAILAND', 'CAMBODIA', 'KOREA'];
   var responseCountries = 0;
   var responseCountriesBreak = false;
@@ -114,6 +134,7 @@ if (document.getElementById('aboutme')) {
     counter += 1;
     console.log(counter + '= counter');
     alert ('Sorry ' + userName + ', you\'re wrong! \nTry again!\nYou have: ' + (6 - counter) + ' more tries!');
+}
 }
 
 //document.write ('A: The average hen lays one egg approximately every 26 hours, which is about 265 eggs per year.');
