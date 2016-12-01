a8eded4066bf53cd3dc9666740d5a55511911c75
juanchaves <chavesjuanm@gmail.com>
2016-11-30 17:38:40 -0800

    messed around in Js Q7 trying to break out of while loop

--- app.js
+++ app.js
@@ -9,60 +9,60 @@
 
 if (document.getElementById('aboutme')) {
 
-  // var response1 = prompt('Can you guess what my favorite movies are? (Yes/No)').toLowerCase();
-  // console.log('Can the user guess my favorite movies: ' + response1);
-  //
-  // if (response1 === 'yes' || response1 === 'y') {
-  //   alert('Yes, I love Fight Club, Trainspotting, Inglorious Basterds, and more!');
-  // } else if (response1 === 'no' || response1 === 'n'){
-  //   alert('That\'s ok. You can learn what they are in a bit.');
-  // } else {
-  //   alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
-  // }
-  //
-  // var response2 = prompt('Can you guess what my favorite TV Shows are? (Yes/No)').toLowerCase();
-  // console.log('Can the user guess my favorite tv shows: ' + response2);
-  //
-  // if (response2 === 'yes' || response2 === 'y') {
-  //   alert('Yes, I love It\'s Always Sunny in Philadelphia, Archer, Rick and Morty, and more!');
-  // } else if (response2 === 'no' || response2 === 'n'){
-  //   alert('That\'s ok. You can learn what they are in a bit.');
-  // } else {
-  //   alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
-  // }
-  //
-  // var response3 = prompt('Can you guess what my favorite Books are? (Yes/No)').toLowerCase();
-  // console.log('Can the user guess my favorite books: ' + response3);
-  //
-  // if (response3 === 'yes' || response3 === 'y') {
-  //   alert('Yes, I love The Lord of the Rings Trilogy, The Hobbit, and The Silmarillion, The Harry Potter Series, Ender\'s Game and Series, and more!');
-  // } else if (response3 === 'no' || response3 === 'n'){
-  //   alert('That\'s ok. You can learn what they are in a bit.');
-  // } else {
-  //   alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
-  // }
-  //
-  // var response4 = prompt('Can you guess what my favorite random stuff/media are? (Yes/No)').toLowerCase();
-  // console.log('Can the user guess my favorite random stuff: ' + response4);
-  //
-  // if (response4 === 'yes' || response4 === 'y') {
-  //   alert('Yes, I love Dungeons and Dragons (D&D) related things (i.e. books, blurbs, etc.), Critical Role -online D&D adventure (Twitch Show), Random/Useless information acquired by following "internet rabbit-holes", and more!');
-  // } else if (response4 === 'no' || response4 === 'n'){
-  //   alert('That\'s ok. You can learn what they are in a bit.');
-  // } else {
-  //   alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
-  // }
-  //
-  // var response5 = prompt('Can you tell me what your favorite things are? (Yes/No)').toLowerCase();
-  // console.log('Can the user guess my favorite movies: ' + response5);
-  //
-  // if (response5 === 'yes' || response5 === 'y') {
-  //   document.write(response5 + ' <--- Wow! These seem pretty cool!');
-  // } else if (response5 === 'no' || response5 === 'n'){
-  //   document.write('That\'s ok, you don\'t wanna share, I understand.');
-  // } else {
-  //   alert ('Sorry, you gotta answer yes or no. \n :-P');
-  // }
+  var response1 = prompt('Can you guess what my favorite movies are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite movies: ' + response1);
+
+  if (response1 === 'yes' || response1 === 'y') {
+    alert('Yes, I love Fight Club, Trainspotting, Inglorious Basterds, and more!');
+  } else if (response1 === 'no' || response1 === 'n'){
+    alert('That\'s ok. You can learn what they are in a bit.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
+  }
+
+  var response2 = prompt('Can you guess what my favorite TV Shows are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite tv shows: ' + response2);
+
+  if (response2 === 'yes' || response2 === 'y') {
+    alert('Yes, I love It\'s Always Sunny in Philadelphia, Archer, Rick and Morty, and more!');
+  } else if (response2 === 'no' || response2 === 'n'){
+    alert('That\'s ok. You can learn what they are in a bit.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
+  }
+
+  var response3 = prompt('Can you guess what my favorite Books are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite books: ' + response3);
+
+  if (response3 === 'yes' || response3 === 'y') {
+    alert('Yes, I love The Lord of the Rings Trilogy, The Hobbit, and The Silmarillion, The Harry Potter Series, Ender\'s Game and Series, and more!');
+  } else if (response3 === 'no' || response3 === 'n'){
+    alert('That\'s ok. You can learn what they are in a bit.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
+  }
+
+  var response4 = prompt('Can you guess what my favorite random stuff/media are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite random stuff: ' + response4);
+
+  if (response4 === 'yes' || response4 === 'y') {
+    alert('Yes, I love Dungeons and Dragons (D&D) related things (i.e. books, blurbs, etc.), Critical Role -online D&D adventure (Twitch Show), Random/Useless information acquired by following "internet rabbit-holes", and more!');
+  } else if (response4 === 'no' || response4 === 'n'){
+    alert('That\'s ok. You can learn what they are in a bit.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n You Lose :,-(');
+  }
+
+  var response5 = prompt('Can you tell me what your favorite things are? (Yes/No)').toLowerCase();
+  console.log('Can the user guess my favorite movies: ' + response5);
+
+  if (response5 === 'yes' || response5 === 'y') {
+    document.write(response5 + ' <--- Wow! These seem pretty cool!');
+  } else if (response5 === 'no' || response5 === 'n'){
+    document.write('That\'s ok, you don\'t wanna share, I understand.');
+  } else {
+    alert ('Sorry, you gotta answer yes or no. \n :-P');
+  }
 
   var response6 = 0;
   var counter = 0;
@@ -80,23 +80,41 @@ if (document.getElementById('aboutme')) {
       alert('Your number is lesser than the answer!');
     }
   }
+  if (response6 !== 26){
+    alert('BTW chickens take 26 hours to lay one egg!');
+  }
   //Question 7
   var countries = ['US', 'ENGLAND', 'FRANCE', 'BELGIUM', 'NETHERLAND', 'GERMANY','ITALY', 'HUNGARY', 'CZECH REPUBLIC', 'ISRAEL', 'CHINA', 'THAILAND', 'CAMBODIA', 'KOREA'];
-  var response7 = 0;
+  var responseCountries = 0;
+  var responseCountriesTrue = true
+  }
+  }
   counter = 0;
+  var tallyNum= 0;
+
   while(counter < 6){
-    response7 = prompt('Can you guess the countries I have traveled to?\nTip:').toUpperCase();
-    counter += 1;
-    for (var i = 0; i < countries.length; i++){
-      if(response7 === countries[i]){
+    responseCountries = prompt('Can you guess the countries I have traveled to?\nTip: Use the correct names').toUpperCase();
+
+
+    for (var i = 0; i < countries.length; i++) {
+
+      if(responseCountries === countries[i]) {
         console.log(countries[i]);
         alert ('Hey, you guessed one! \nHere are the other places I have been: ' + countries);
-      }else{
-        alert('Try again!');
+        counter = 9;
+        break;
+
       }
+
+
+        counter += 1;
+
+      }
+
+
     }
   }
-}
+
 //document.write ('A: The average hen lays one egg approximately every 26 hours, which is about 265 eggs per year.');
 //Useful and descriptive console.log() messages in the JS are well written and correctly displaying to the browser console for each question of the guessing game.
 
